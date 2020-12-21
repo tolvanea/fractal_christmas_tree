0ea94379b8a5ea56dbbd4f3299d623fc48c742a5
tolvanea <alpi.tolvanen@tutanota.com>
2020-12-21 03:19:24 +0200

    Render with numbda

--- barnsley_tree.py
+++ barnsley_tree.py
@@ -1,16 +1,27 @@
 import numpy as np
 from PIL import Image, ImageDraw  # Run 'pip3 install pillow'
+from numba import jit
+from numba.typed import List
 w, h = 1280, 1920  # width, height
 data = [([[ 0.1,  0.00],[ 0.00, 0.2]], [0.0, 0.3]),     # trunk
         ([[ 0.1,  0.00],[ 0.00, 0.2]], [0.0, 0.37]),    # trunk
         ([[ 0.87, 0.01],[-0.01, 0.87]], [0.0, 0.8]),    # copy branches
         ([[ 0.30,-0.3], [ 0.70, -0.2]], [0.0, 0.3]),    # left branch
         ([[-0.30, 0.3], [ 0.70, -0.2]], [-0.0, 0.3])]   # right branch
+@jit(nopython=True)
 def draw_tree(affines):
     img = np.zeros((h, w, 3), dtype=np.uint32)
     r = [np.array([0.0, 0.0]), np.array([0.0, 0.0])]
     for i in range(w*h*10):
-        idx = np.random.choice([0, 1, 2, 3], p=[0.02, 0.02, 0.76, 0.2])
+        rnd = np.random.rand()
+        if rnd < 0.02:
+            idx = 0
+        elif rnd < 0.04:
+            idx = 1
+        elif rnd < 0.8:
+            idx = 2
+        else:
+            idx = 3
         if idx == 3:
             r[1] = r[0] # Copy left branch to right side
         for j in range(2):
@@ -19,7 +30,7 @@ def draw_tree(affines):
             x, y = int(w * (r[j][0]/3.5 - 0.5)), int(h * (r[j][1]/7.5 + 0.05))
             img[y, x, :] += np.array([1,8,1], dtype=np.uint8)
     return img
-data_numpy = list(map(lambda t: (np.array(t[0]), np.array(t[1])), data))
+data_numpy = List(map(lambda t: (np.array(t[0]), np.array(t[1])), data))
 img = draw_tree(data_numpy)
 img[...] = np.sqrt(img / img.max()) * 255
 img[:h//17, :] = 0; img[-h//10:, :] = 0
